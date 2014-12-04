1f297513a1b05d844a00437ad1af998ed664f961
Thomas ten Cate <ttencate@gmail.com>
2014-12-04 20:24:54 +0100

    Add howler.js for sound effects

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 /// <reference path="../typings/tsd.d.ts" />
 
+class Assets {
+  jump: Howl = new Howl({urls: ['jump.mp3', 'jump.ogg']});
+}
+
 class Game {
   context: CanvasRenderingContext2D;
   canvasWidth: number;
   canvasHeight: number;
 
-  constructor(private canvas: HTMLCanvasElement) {
+  constructor(private assets: Assets, private canvas: HTMLCanvasElement) {
     this.context = canvas.getContext('2d');
     this.canvasWidth = canvas.width;
     this.canvasHeight = canvas.height;
@@ -14,7 +18,9 @@ class Game {
   run() {
     this.context.fillStyle = '#fed';
     this.context.fillRect(0, 0, this.canvasWidth, this.canvasHeight);
+
+    this.assets.jump.play();
   }
 }
 
-new Game(<HTMLCanvasElement>$('canvas')[0]).run();
+new Game(new Assets(), <HTMLCanvasElement>$('canvas')[0]).run();
