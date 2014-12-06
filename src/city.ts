ebdab80e2a939cdd24c6f44787248333912b53bd
Thomas ten Cate <ttencate@gmail.com>
2014-12-06 07:58:07 +0100

    Show the grid

--- src/city.ts
+++ src/city.ts
@@ -1,4 +1,30 @@
 /// <reference path="../typings/tsd.d.ts" />
 
+enum CellType {
+  GRASS,
+  HOUSE,
+}
+
+class Cell {
+  type: CellType;
+  variant: number;
+}
+
 class City {
+  width = 32;
+  height = 32;
+  grid: Array<Array<Cell>>;
+
+  constructor() {
+    this.grid = [];
+    for (var i = 0; i < this.height; i++) {
+      this.grid[i] = [];
+      for (var j = 0; j < this.width; j++) {
+        var cell = new Cell();
+        cell.type = CellType.GRASS;
+        cell.variant = 0;
+        this.grid[i][j] = cell;
+      }
+    }
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,15 @@ window.requestAnimationFrame =
   window['mozRequestAnimationFrame'] ||
   function(callback) { window.setTimeout(callback, 1000 / 60); };
 
+function loadImage(url: string) {
+  var img = <HTMLImageElement>document.createElement('img');
+  img.src = url;
+  return img;
+}
+
 class Assets {
   jump: Howl = new Howl({urls: ['jump.mp3', 'jump.ogg']});
+  sprites: HTMLImageElement = loadImage('sprites.png');
 }
 
 var MARGIN = 20;
@@ -15,10 +22,13 @@ var MARGIN = 20;
 class Game {
   context: CanvasRenderingContext2D;
   canvasElt: HTMLCanvasElement;
+
   canvasWidth: number;
   canvasHeight: number;
+  cellWidth: number;
+  cellHeight: number;
 
-  city: City;
+  city: City = new City();
 
   constructor(private assets: Assets, private canvas: JQuery) {
     this.canvasElt = <HTMLCanvasElement>canvas[0];
@@ -26,8 +36,6 @@ class Game {
 
     $(window).on('resize', this.onResize.bind(this));
     this.onResize();
-
-    this.city = new City();
   }
 
   private onResize() {
@@ -42,6 +50,12 @@ class Game {
     h = Math.floor(h);
     this.canvasElt.width = this.canvasWidth = w;
     this.canvasElt.height = this.canvasHeight = h;
+    $('#all').css({
+      width: w + 'px',
+      height: h + 'px',
+    });
+    this.cellWidth = w / Math.max(this.city.width, this.city.height);
+    this.cellHeight = this.cellWidth / 2;
     $(document.documentElement).css('font-size', w/100 + 'px');
   }
 
@@ -51,7 +65,28 @@ class Game {
 
   private frame() {
     this.context.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
+
+    for (var i = 0; i < this.city.height; i++) {
+      for (var j = 0; j < this.city.width; j++) {
+        this.drawTile(i, j, this.city.grid[i][j]);
+      }
+    }
+
     requestAnimationFrame(this.frame.bind(this));
+  }
+
+  private drawTile(i: number, j: number, cell: Cell) {
+    var x = this.getCenterX(i, j);
+    var y = this.getCenterY(i, j);
+    this.context.drawImage(this.assets.sprites, 0, 0, 40, 20, x - this.cellWidth / 2, y - this.cellHeight / 2, this.cellWidth, this.cellHeight);
+  }
+
+  private getCenterX(i: number, j: number) {
+    return this.canvasWidth / 2 + this.cellWidth / 2 * (i + 0.5 - this.city.height / 2 + j + 0.5 - this.city.width / 2);
+  }
+
+  private getCenterY(i: number, j: number) {
+    return this.canvasHeight / 2 + this.cellHeight / 2 * (i+ 0.5  - this.city.height / 2 - j - 0.5 + this.city.width / 2);
   }
 }
 
